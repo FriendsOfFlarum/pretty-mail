45d2c77696fdd3a5481e69cf1200c2b0dc9e5d5f
Eddie Webb <ollitech@gmail.com>
2021-04-14 14:47:16 -0400

    appease the linter

--- src/Providers/MailerProvider.php
+++ src/Providers/MailerProvider.php
@@ -3,10 +3,11 @@
 /*
  * This file is part of fof/pretty-mail.
  *
- * Copyright (c) 2019 FriendsOfFlarum.
+ * Copyright (c) FriendsOfFlarum.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
  */
 
 namespace FoF\PrettyMail\Providers;
@@ -14,7 +15,6 @@ namespace FoF\PrettyMail\Providers;
 use Flarum\Foundation\AbstractServiceProvider;
 use Flarum\Notification\NotificationMailer;
 use Flarum\Settings\SettingsRepositoryInterface;
-use FoF\PrettyMail\Mailer;
 use FoF\PrettyMail\Overrides;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Mail\MailManager;
@@ -28,14 +28,14 @@ class MailerProvider extends AbstractServiceProvider
             return new MailManager($container);
         });
 
-        $this->app->bind('mailer',function(Container $container) {
+        $this->app->bind('mailer', function(Container $container) {
             $mailer = $container->make('mail.manager')->mailer();
             $settings = app(SettingsRepositoryInterface::class);
             $mailer->alwaysFrom($settings->get('mail_from'), $settings->get('forum_title'));
+
             return $mailer;
         });
             
-
         $this->app->extend(NotificationMailer::class, function (NotificationMailer $mailer) {
             return app(Overrides\NotificationMailer::class);
         });
